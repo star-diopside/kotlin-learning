b89694114770528f2596e86bd606608b726fcbcb
se-yoshi <se.yoshi@gmail.com>
2018-05-26 19:07:34 +0900

    プロジェクトの雛形を作成

---
+++ sound/src/main/kotlin/jp/gr/java_conf/stardiopside/ktlearn/sound/KtlearnApplication.kt
@@ -0,0 +1,11 @@
+package jp.gr.java_conf.stardiopside.ktlearn.sound
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KtlearnApplication
+
+fun main(args: Array<String>) {
+    runApplication<KtlearnApplication>(*args)
+}
